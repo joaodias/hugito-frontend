9986ef063be6607d9c12ae53e4676b5c49e46655
Joao Dias <diasjoaoac@gmail.com>
2016-07-24 20:12:11 +0100

    Refactor TopbarSection
    To support the controls section.
    
    Issue #9

--- components/topbar/TopbarSection.jsx
+++ components/topbar/TopbarSection.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import RepositorySection from './RepositorySection.jsx';
 import UserSection from './UserSection.jsx';
+import ControlSection from './ControlSection.jsx'
 
 class TopbarSection extends Component{
     render(){
@@ -8,6 +9,7 @@ class TopbarSection extends Component{
             <div id="topbar">
                 <RepositorySection {...this.props}/>
                 <UserSection {...this.props}/>
+                <ControlSection {...this.props}/>
             </div>
         )
     }
@@ -17,7 +19,8 @@ TopbarSection.propTypes = {
     userName: React.PropTypes.object.isRequired,
     setUserName: React.PropTypes.func.isRequired,
     repositories: React.PropTypes.array.isRequired,
-    setRepository: React.PropTypes.func.isRequired
+    setRepository: React.PropTypes.func.isRequired,
+    showContent: React.PropTypes.object.isRequired
 }
 
 export default TopbarSection
