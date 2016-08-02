d24d74a56a900f9484fb0e97615d7e6a5b22eed8
Joao Dias <diasjoaoac@gmail.com>
2016-08-02 11:43:33 +0100

    Add React Router
    Issue #16

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactRouter, { Router, Route, browserHistory } from 'react-router'
+import Login from './components/Login.jsx'
 import App from './components/App.jsx'
+import MainSection from './components/main/MainSection.jsx'
+import ContentList from './components/main/ContentList.jsx'
+import ContentEditor from './components/main/ContentEditor.jsx'
 
 ReactDOM.render(
-    <App />,
+    <Router history={browserHistory}>
+        <Route path="/" component={Login}></Route>
+        <Route path="/dashboard" component={App}></Route>
+    </Router>,
     document.getElementById('app')
 );
