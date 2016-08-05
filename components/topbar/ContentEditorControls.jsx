c1daa66752863ce77f1400c971ae1b1f91d34bf9
Joao Dias <diasjoaoac@gmail.com>
2016-08-05 17:31:15 +0100

    Add back arrow
    Issue #28

--- components/topbar/ContentEditorControls.jsx
+++ components/topbar/ContentEditorControls.jsx
@@ -3,9 +3,16 @@ import { Button } from 'react-bootstrap'
 import { Glyphicon } from 'react-bootstrap'
 
 class ContentEditorControls extends Component{
+    onClick(e){
+        // TODO: Handle different buttons
+        e.preventDefault();
+        this.props.setShowContentEditor({value: false});
+        this.props.setShowContent({value: true});
+    }
     render(){
         return (
             <ul id="content-editor-controls">
+                <li><Button bsSize="small" className="btn" onClick={this.onClick.bind(this)}><Glyphicon glyph="arrow-left"/></Button></li>
                 <li><h5>{this.props.fileName.value}</h5></li>
                 <li><Button bsSize="small" className="btn"><Glyphicon glyph="eye-open"/> Preview</Button></li>
                 <li><Button bsSize="small" className="btn"><Glyphicon glyph="upload"/> Publish</Button></li>
@@ -15,7 +22,9 @@ class ContentEditorControls extends Component{
 }
 
 ContentEditorControls.propTypes = {
-    fileName: React.PropTypes.object.isRequired
+    fileName: React.PropTypes.object.isRequired,
+    setShowContent: React.PropTypes.func.isRequired,
+    setShowContentEditor: React.PropTypes.func.isRequired
 }
 
 export default ContentEditorControls
